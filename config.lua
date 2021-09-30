7f502e982ecd08f0e16223f6c36fb50e060f013f
Eralp <eralpozcans@gmail.com>
2021-10-01 00:14:37 +0300

    add CircleZones example
    add CircleZones example

--- config.lua
+++ config.lua
@@ -3,7 +3,31 @@ local Config, Zones, PlayerData = {}, {}, {}
 Config.Debug = false
 
 Config.CircleZones = {
-
+    ["Circletest"] = {
+        name = 'CircleTest1',
+        coords = vector3(-258.17, -978.03, 31.22),
+        radius = 1.5,
+        debugPoly = false,
+        useZ = false,
+        menuoptions = {
+            buttons = {
+                {
+                    type = 'header',
+                    title = 'Titles',
+                    description = 'Alt Title'
+                },
+                {
+                    type = "client", -- client/server/command/qbcommand
+                    event = "eventname",
+                    options = {
+                        data = "chips", -- Send values. args.
+                    },
+                    title = 'Fist Title',
+                    description = 'Title'
+                }
+            }
+        }
+    },
 }
 
 Config.BoxZones = {
