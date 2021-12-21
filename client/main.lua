6dc86c7023381a166633212723a1127f5cc49db9
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-12-21 11:57:37 +0100

    feat(client): Add more security

--- client/main.lua
+++ client/main.lua
@@ -1,16 +1,21 @@
 local headerShown = false
+local sendData = nil
+
+-- Functions
 
 local function openMenu(data)
-    if not data then return end
+    if not data or not next(data) then return end
     SetNuiFocus(true, true)
     headerShown = false
+    sendData = data
     SendNUIMessage({
         action = 'OPEN_MENU',
-        data = data
+        data = table.clone(data)
     })
 end
 
 local function closeMenu()
+    sendData = nil
     headerShown = false
     SetNuiFocus(false)
     SendNUIMessage({
@@ -19,43 +24,59 @@ local function closeMenu()
 end
 
 local function showHeader(data)
+    if not data or not next(data) then return end
     headerShown = true
+    sendData = data
     SendNUIMessage({
         action = 'SHOW_HEADER',
-        data = data
+        data = table.clone(data)
     })
 end
+
+-- Events
 
 RegisterNetEvent('qb-menu:client:openMenu', function(data)
     openMenu(data)
 end)
+
 RegisterNetEvent('qb-menu:client:closeMenu', function()
     closeMenu()
 end)
 
-RegisterNUICallback('clickedButton', function(data)
+-- NUI Callbacks
+
+RegisterNUICallback('clickedButton', function(option)
     if headerShown then headerShown = false end
-    PlaySoundFrontend(-1, 'Highlight_Cancel','DLC_HEIST_PLANNING_BOARD_SOUNDS', 1)
+    PlaySoundFrontend(-1, 'Highlight_Cancel', 'DLC_HEIST_PLANNING_BOARD_SOUNDS', 1)
     SetNuiFocus(false)
-    if data.event then
-        if data.isServer then
-            TriggerServerEvent(data.event, data.args)
-        elseif data.isCommand then
-            ExecuteCommand(data.event)
-        elseif data.isQBCommand then
-            TriggerServerEvent('QBCore:CallCommand', data.event, data.args)
-        elseif isAction then
-            data.event(data.args)
-        else
-            TriggerEvent(data.event, data.args)
+    if sendData then
+        local data = sendData[tonumber(option)]
+        sendData = nil
+        if data then
+            if data.params.event then
+                if data.params.isServer then
+                    TriggerServerEvent(data.params.event, data.params.args)
+                elseif data.params.isCommand then
+                    ExecuteCommand(data.params.event)
+                elseif data.params.isQBCommand then
+                    TriggerServerEvent('QBCore:CallCommand', data.params.event, data.params.args)
+                elseif data.params.isAction then
+                    data.params.event(data.params.args)
+                else
+                    TriggerEvent(data.params.event, data.params.args)
+                end
+            end
         end
     end
 end)
 
 RegisterNUICallback('closeMenu', function()
     headerShown = false
+    sendData = nil
     SetNuiFocus(false)
 end)
+
+-- Command and Keymapping
 
 RegisterCommand('+playerfocus', function()
     if headerShown then
@@ -65,7 +86,8 @@ end)
 
 RegisterKeyMapping('+playerFocus', 'Give Menu Focus', 'keyboard', 'LMENU')
 
+-- Exports
+
 exports('openMenu', openMenu)
 exports('closeMenu', closeMenu)
 exports('showHeader', showHeader)
-exports('clearHistory', clearHistory)
